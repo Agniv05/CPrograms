52f74beb0cdbee26b70c18a7d2297a9959a7f5a6
Anwesha <122878493+Agniv05@users.noreply.github.com>
2024-06-20 12:31:04 +0530

    Create prime.c
    checks for prime no

---
+++ prime.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+// Function to check if a number is prime
+bool isPrime(int n) {
+    if (n <= 1) {
+        return false;
+    }
+    for (int i = 2; i <= n / 2; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+// Main function to test the isPrime function
+int main() {
+    int number = 17; // You can change this to test different numbers
+
+    if (isPrime(number)) {
+        printf("%d is a prime number.\n", number);
+    } else {
+        printf("%d is not a prime number.\n", number);
+    }
+
+    return 0;
+}
