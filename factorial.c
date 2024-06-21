927742a8cd04cf14b71fcc356aad52efe47649f4
Anwesha <122878493+Agniv05@users.noreply.github.com>
2024-06-21 16:30:08 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -1,1 +1,15 @@
-
+#include <stdio.h>
+int fact(int x){
+  if (x<=1){
+    return 1;
+  }
+  return (x*fact(x-1));
+}
+int main(){
+  int a;
+  printf ("Enter no. :");
+  scanf ("%d",&a);
+  int f=fact(a);
+  printf ("factorial = %d",f);
+  return 0;
+}
