ff7351dd7466cccf4d06b90d139e099fe2413ad8
Anwesha <122878493+Agniv05@users.noreply.github.com>
2024-06-28 20:16:16 +0530

    Update factrec.c

--- factrec.c
+++ factrec.c
@@ -1,2 +1,16 @@
+int recpor(int a){
+  if (a==1){
+    return 1;
+  }
+  else{
+    return a*recpor(a-1);
+  }
+}
 int main(){
+  int n;
+  printf ("enter no.");
+  scanf ("%d",&n);
+  int b=recpor(n);
+  printf ("factorial= %d",b);
+  return 0;
 }
