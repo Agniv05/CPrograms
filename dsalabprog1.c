f76be4579b1b021525b3e3442f5176d7be61bb38
Agniv <122878493+Agniv05@users.noreply.github.com>
2024-08-05 11:15:10 +0530

    Update dsalabprog1.c

--- dsalabprog1.c
+++ dsalabprog1.c
@@ -1,1 +1,38 @@
-
+#include <stdio.h>
+int main(){
+    int size;
+    printf ("Enter size of array : ");
+    scanf ("%d",&size);
+    int arr[size];
+    printf ("Enter elements of array :");
+    for (int i=0;i<size;i++){
+        scanf ("%d", &arr[i]);
+    }
+    int e;
+    printf ("Enter element to search :");
+    scanf ("%d", &e);
+    int min=arr[0];
+    int t=-1;
+    for (int j=0;j<size;j++){
+        if (arr[j]==e){
+            t=j;
+        }
+        if (arr[j]<min){
+            min=arr[j];
+        }
+    }
+    if (t<0){
+        printf ("Element not found. ");
+        printf ("\n");
+    }
+    else{
+        printf ("Position of element is : %d",t);
+        printf ("\n");
+        if (min<arr[t]){
+            printf ("It is not smallest element. ");
+        }
+        else{
+            printf ("It is smallest element. ");
+        }
+    }
+}
